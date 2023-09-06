cbd8a7a1aa5f0325c4337c54950b5d9c3bdbeaf1
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-09-06 15:51:11 +0200

    06.09.2023-updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.js-slider').slick({
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
@@ -8,8 +8,8 @@ $(document).ready(function(){
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 2,
+                slidesToScroll: 1,
                 infinite: true,
                 dots: true
               }
@@ -17,8 +17,8 @@ $(document).ready(function(){
             {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1
               }
             },
             {
