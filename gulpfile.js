1b3e65a22f86772f8417bb600067b9169b85b605
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-09-19 13:51:33 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@
 var gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function () { gulp.src('assets/scss/style.scss') .pipe(sass().on('error', sass.logError)) .pipe(gulp.dest('assets/css')); });
-gulp.task('sass:watch', function() { gulp.watch('assets/scss/style.scss', ['sass']);});
+gulp.task('sass', function () { gulp.src('./assets/scss/style.scss') .pipe(sass().on('error', sass.logError)) .pipe(gulp.dest('./assets/css')); });
+gulp.task('sass:watch', function() { gulp.watch('./assets/scss/style.scss', ['sass']);});
