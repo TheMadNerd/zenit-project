eb4cd865331f3b3774922831408886c4bfc3a323
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-09-19 11:44:46 +0200

    mobile-nav-bar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(){  //js-slider
     $('.js-slider').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
@@ -31,3 +31,28 @@ $(document).ready(function(){
         ]
     });
   });
+
+
+  
+window.onscroll = function() {myFunction()};  //sticky menu 
+
+var navbar = document.getElementById("menu");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+const menuBtn = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav-bar');
+
+menuBtn.addEventListener('click', function() {
+  menuBtn.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
+})
